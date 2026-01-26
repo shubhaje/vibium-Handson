4e6d3517e227afec400719801fb1555c3958d204
shubhaje <shubhangi.ajegaonkar@gmail.com>
2026-01-26 22:49:06 +0100

    config details

---
+++ config.py
@@ -0,0 +1,12 @@
+project_name: "vibium"
+version: "1.0.0"
+description: "Browser automation testing with vibium"
+browser:
+  headless: true
+  timeout: 30
+  width: 1280
+  height: 720
+screenshot:
+  folder: "screenshots"
+  format: "png"
+  quality: 95
